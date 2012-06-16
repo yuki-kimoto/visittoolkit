b860dd8166208c254a2e8d7775ea65025b1c683a
Yuki Kimoto <kimoto.yuki@gmail.com>
2012-06-16 17:51:52 +0900

    管理者の登録画面を追加

---
+++ public/js/visittoolkitapi.js
@@ -0,0 +1,19 @@
+var visittoolkitapi = {};
+visittoolkitapi.create_form_data = function (selector) {
+  var form = $(selector);
+  var params = {};
+  form.find('input').each(function () {
+    var type = $(this).attr("type") || '';
+    if (type === '' || type === 'password' || type === 'hidden') {
+      var name = $(this).attr('name');
+      params[name] = $(this).val();
+    }
+  });
+  
+  form.find('textarea').each(function () {
+    var name = $(this).attr('name');
+    params[name] = $(this).val();
+  });
+  
+  return params;
+};
